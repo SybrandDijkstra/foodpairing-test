3d0d01f4757e05fb9c3c706fe13ca454d89ba96f
FOODPAIRING\Sybrand <sybrand.dijkstra@pandora.be>
2023-01-09 14:26:17 +0100

    Added poetry files

---
+++ demo/my_python_file.py
@@ -0,0 +1,18 @@
+PARTICIPANTS = [
+    'Abhisheik',
+    'Junaid',
+    'Miguel',
+    'Padhmanand',
+    'Daan',
+    'Marc',
+    'Stijn',
+]
+
+
+def main():
+    for participant in PARTICIPANTS:
+        print(participant)
+
+
+if __name__ == '__main__':
+    main()
